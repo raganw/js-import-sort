2f5b832333c56044792bfa5ac8600e7d45d0ec5a
Ragan Webber <ragan.webber@socrata.com>
2018-03-07 16:14:59 -0800

    testing

--- __testfixtures__/FlowTypes.input.js
+++ __testfixtures__/FlowTypes.input.js
@@ -2,3 +2,9 @@
 import type {Thing} from 'Something';
 
 import {OtherThing} from 'Something';
+
+type Foo<T, A> = {
+  bar: (arg: T<A>) => void,
+  foo: mixed[],
+  baz: { [key: string]: string }
+};
--- __testfixtures__/FlowTypes.output.js
+++ __testfixtures__/FlowTypes.output.js
@@ -1,3 +1,7 @@
 import { OtherThing } from 'Something';
 
 import type { Thing } from 'Something';
+
+type Foo<T, A> = {
+  bar: T<A>
+};
